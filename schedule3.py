bd2cbd8ef7c2ce15da67e5fc7f4fc3020ca3337c
Meshee <mrachhh@gmail.com>
2013-09-06 21:15:20 -0400

    Update schedule3.py
    Optimization by Nyle.

--- schedule3.py
+++ schedule3.py
@@ -31,26 +31,23 @@ def makeblock (color, daysOfWeek, time, length):
                                 VOFFSET-2+HEIGHT/13*(hours[time]+length),
                                 fill=color)
 
-# Courses array madness
-courses = [[[[0 for modata in range(3)]for classes in range(7)] for data in range(3)] for cours in range(10)]
-
 # Get inputs
 name = input("Your Name: ")
 title = input("Schedule Title: ")
 numCourses = input("Number of Courses: ")
 for iii in range(int(numCourses)):
-    courses[iii][0] = input("Course "+str(iii+1)+" title: ")
-    courses[iii][1] = input("Course "+str(iii+1)+" color: ")
+    title = input("Course "+str(iii+1)+" title: ")
+    color = input("Course "+str(iii+1)+" color: ")
     # display class name and color on side
-    w.create_rectangle(WIDTH-135, VOFFSET+100+20*iii, WIDTH-125, VOFFSET+110+20*iii, fill=courses[iii][1])
-    w.create_text(WIDTH-115, VOFFSET+105+20*iii, anchor="w", text=courses[iii][0], font=("Tahoma", 12))
+    w.create_rectangle(WIDTH-135, VOFFSET+100+20*iii, WIDTH-125, VOFFSET+110+20*iii, fill=color)
+    w.create_text(WIDTH-115, VOFFSET+105+20*iii, anchor="w", text=title, font=("Tahoma", 12))
     diffTimes = input("How many different times of the day during the week do you have this course? "
                          +"(ex. 10'o'clocks on MTRF and a 2'o'clock on W would be only be '2': ")
     for jjj in range(int(diffTimes)):
-        courses[iii][2][jjj][0] = input("Class #"+str(jjj+1)+" start time (integer only. ex. '9'): ")
-        courses[iii][2][jjj][1] = input("Class #"+str(jjj+1)+" length in hours (integer only): ")
-        courses[iii][2][jjj][2] = input("Days of week for class #"+str(jjj+1)+": (ex. 'MTRF'): ")
-        makeblock(courses[iii][1], courses[iii][2][jjj][2], int(courses[iii][2][jjj][0]), int(courses[iii][2][jjj][1]))
+        start = input("Class #"+str(jjj+1)+" start time (integer only. ex. '9'): ")
+        length = input("Class #"+str(jjj+1)+" length in hours (integer only): ")
+        days = input("Days of week for class #"+str(jjj+1)+": (ex. 'MTRF'): ")
+        makeblock(color, days, int(start), int(length))
 
 w.create_text((WIDTH+5)/2, 35, text=name+" - "+title, font=("Tahoma", 40))
 print ("Done. Screenshot your schedule")
